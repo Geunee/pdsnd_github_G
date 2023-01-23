a5710972f66674e1229e3b64280bdd02fc5550ab
Geun <calix9k@gmail.com>
2023-01-23 14:34:15 +0900

    Add lines in bikeshare_final.py ..

--- bikeshare_final.py
+++ bikeshare_final.py
@@ -2,6 +2,11 @@ import time
 import pandas as pd
 import numpy as np
 
+#update
+
+#smt changes
+#we need to add city later...
+
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
